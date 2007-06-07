be327db7e6fa2ca0bbcbfddabf2b6e693910c6a4
Kristian Lyngstøl <kristian@albus.(none)>
2007-06-07 17:31:17 +0200

    Disable cursor zoom if the head the cursor is on is not zoomed.

--- zoom.c
+++ zoom.c
@@ -593,6 +593,35 @@ syncCenterToMouse (CompScreen *s)
     } 
 }
 
+/* Check if zoom is active on the output specified */
+static Bool
+isActive (CompScreen *s, int out)
+{
+    ZOOM_SCREEN (s);
+    if (!zs->grabbed)
+	return FALSE;
+    if (zs->zooms[out].currentZoom == 1.0f &&
+	zs->zooms[out].newZoom == 1.0f &&
+	zs->zooms[out].zVelocity == 0.0f)
+	return FALSE;
+    return TRUE;
+}
+
+/* Check if the cursor is still visible.
+ */
+static void 
+cursorMoved (CompScreen *s)
+{
+    ZOOM_SCREEN (s);
+    int out;
+    if (!zs->grabbed)
+	return ;
+    out = outputDeviceForPoint (s, zs->mouseX, zs->mouseY);
+    if (isActive (s, out))
+	cursorZoomActive (s);
+    else
+	cursorZoomInactive (s);
+}
 /* Update the mouse position.
  * Based on the zoom engine in use, we will have to move the zoom area.
  * This might have to be added to a timer. 
@@ -622,6 +651,7 @@ updateMousePosition (CompScreen *s)
 	    zs->lastChange = time(NULL);
 	    setCenter (s, rootX, rootY, TRUE);
 	}
+	cursorMoved (s);
 	damageScreen (s);
     }
 }
