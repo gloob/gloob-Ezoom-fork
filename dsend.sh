fbb403d32881b3a09bfbf2227db79fc17a2b2aa1
Kristian Lyngstol <kristian@bohemians.org>
2007-06-15 22:37:35 +0200

    Start the API work

---
+++ dsend.sh
@@ -0,0 +1,3 @@
+#!/bin/sh 
+# Sends a dbus option to a plugin
+dbus-send --type=method_call --dest=org.freedesktop.compiz /org/freedesktop/compiz/$1/allscreens/$2 org.freedesktop.compiz.activate string:'root' int32:`xwininfo -root | grep id: | awk '{ print $4 }'` $3 $4 $5 $6 $7 $8 $9 
